d8078316af09891b4bb221fef9635f4c7ba6d153
slu000uls <sitesoheil@gmail.com>
2023-09-14 18:05:15 +0430

    delete line

--- php_to_js.php
+++ php_to_js.php
@@ -4,7 +4,6 @@ class php_to_js {
     public function displayVariable($variable_name,$value) {
         echo "<script>";
         echo "var $variable_name = " . json_encode($value) . ";";
-        // echo "console.log(myVariable);";
         echo "</script>";
     }
 }

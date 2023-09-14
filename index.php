71d72af4a1cf9b7efa971c9c857c84317bf04158
slu000uls <sitesoheil@gmail.com>
2023-09-14 18:06:08 +0430

    change word

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include "./php_to_js.php";
 //we create object for this class.
 $php_to_js = new php_to_js();
 //we enter the Variable and put value inside it.
-$php_to_js->displayVariable("fromphp","this value comes from php");
+$php_to_js->displayVariable("fromphp","this value its from php");
 
 //you can use this Variable in js like this :
 //console.log(fromphp);
